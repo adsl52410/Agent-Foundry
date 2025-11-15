3451ec3202b355f3728c9fc7d016ed6a76bb4e5a
Neil Lu <adsl52410@gmail.com>
2025-11-15 15:13:01 +0800

    Update project metadata and dependencies: Rename package to 'agent-foundry', set version to 0.1.0, and define project dependencies in pyproject.toml and requirements-dev.txt.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="afm",
+    name="agent-foundry",
     version="0.1.0",
     description="Agent Foundry Manager: LLM Agent toolkit manager",
     long_description=long_description,
